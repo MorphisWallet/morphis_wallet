c31850a859cd8837c3dbf137156d5f282910b8f5
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-07 18:17:32 +0800

    feat(): distinguish LayoutLogin and LayoutMain

---
+++ packages/morphis/src/pages/layout/layout_login/LayoutLogin.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from 'react'
+import cl from 'classnames'
+
+import st from './LayoutLogin.module.less'
+
+type LayoutLoginProps = {
+  header?: ReactNode | ReactNode[]
+  children: ReactNode | ReactNode[]
+  footer?: ReactNode | ReactNode[]
+  className?: string
+}
+
+export const LayoutLogin = ({
+  header,
+  children,
+  footer,
+  className,
+}: LayoutLoginProps) => (
+  <div
+    className={cl([st.layout, !header && !footer && st.mainOnly, className])}
+  >
+    {header && <div className={st.header}>{header}</div>}
+    <div className={st.main}>{children}</div>
+    {footer && <div className={st.footer}>{footer}</div>}
+  </div>
+)
