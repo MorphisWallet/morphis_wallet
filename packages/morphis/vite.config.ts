6a897218edc0cd951a07aee3a34c3acfdce5c672
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-08 15:49:38 +0800

    feat(): add svgr dependency

--- packages/morphis/vite.config.ts
+++ packages/morphis/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +14,10 @@ export default defineConfig({
       '@layouts': resolve(__dirname, '/src/layouts'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg',
+    }),
+  ],
 })
