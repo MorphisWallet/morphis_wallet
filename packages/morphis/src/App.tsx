571ceef375f8d26068858e0d0ee00d4753fb4a24
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-09 18:21:50 +0800

    chore(): detach router and theme logic from App

--- packages/morphis/src/App.tsx
+++ packages/morphis/src/App.tsx
@@ -1,73 +1,20 @@
+import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
 
-import { Welcome } from '@pages/welcome'
-import { Create } from '@pages/create'
-import { Done } from '@pages/create/pages/done'
-import { Unlock } from '@pages/unlock'
-import { Landing } from '@pages/landing'
-import { NFT } from '@pages/nft'
-import { History } from '@pages/history'
-import { Settings } from '@pages/settings'
-
-import { Loading } from '@components/loading'
+import { store } from '@core/store'
+import theme from './theme'
+import router from './router'
 
 import './App.less'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#000',
-    },
-    secondary: {
-      main: '#fff',
-    },
-  },
-  typography: {
-    fontFamily: 'satoshi',
-  },
-})
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Welcome />,
-  },
-  {
-    path: 'create',
-    element: <Create />,
-  },
-  {
-    path: 'create/done',
-    element: <Done />,
-  },
-  {
-    path: 'unlock',
-    element: <Unlock />,
-  },
-  {
-    path: 'landing',
-    element: <Landing />,
-  },
-  {
-    path: 'nft',
-    element: <NFT />,
-  },
-  {
-    path: 'history',
-    element: <History />,
-  },
-  {
-    path: 'settings',
-    element: <Settings />,
-  },
-])
 
 const App = () => (
   <IntlProvider locale="en">
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   </IntlProvider>
 )
---
+++ packages/morphis/src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+    secondary: {
+      main: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: 'satoshi',
+  },
+})
