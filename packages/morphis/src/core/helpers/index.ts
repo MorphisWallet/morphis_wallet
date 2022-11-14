a8f5732c7a84dfe1a466332678df6cf655031a81
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-14 18:50:27 +0800

    refactor(): reset repo

--- packages/morphis/src/core/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-export { default as formatDate } from './formatDate'
-export { default as notEmpty } from './notEmptyCheck'
